9016d13f7838f3d7f8323a129423f529a674f63b
Krishanx2003 <aiml21040@glbitm.ac.in>
2024-08-30 23:18:17 +0530

    AuthConnect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -64,10 +64,10 @@ const Login = () => {
     sessionStatus !== "authenticated" && (
       <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Welcome to Aceternity login
+        Welcome to AuthConnect login
       </h2>
       <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Login to aceternity if you can because we don&apos;t have a login flow
+        Login to AuthConnect if you can because we don&apos;t have a login flow
         yet
       </p>
           <form className="my-8" onSubmit={handleSubmit}>
