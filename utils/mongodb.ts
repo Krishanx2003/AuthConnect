2852d59e085e0ecfeec34766fd06afebf86a17e0
Krishanx2003 <aiml21040@glbitm.ac.in>
2024-08-30 23:23:38 +0530

    mongodb

--- utils/mongodb.ts
+++ utils/mongodb.ts
@@ -6,8 +6,7 @@ const connect = async (): Promise<void> => {
 
   try {
     await mongoose.connect(process.env.MONGO_URL as string, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    
     } as ConnectOptions);
     console.log('Mongo Connection successfully established.');
   } catch (error) {
